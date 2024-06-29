81511923dc4982580f3c5cfdefbcfa8d277441ec
luanlazz <luanlazzari@hotmail.com>
2024-06-29 18:58:24 -0300

    chore: configure monorepo structure and apps

--- apps/url-service/src/url-service.service.ts
+++ apps/url-service/src/url-service.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class UrlServiceService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World url!';
   }
 }
