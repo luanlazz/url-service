2e24c72fe149219eaf28293baf9b5edcd12a8b17
luanlazz <luanlazzari@hotmail.com>
2024-06-30 15:13:27 -0300

    chore: user entity

--- apps/user-service/src/user/entities/user.entity.ts
+++ apps/user-service/src/user/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @Column({ primary: true })
+  id: string;
+
+  @Column({ type: 'varchar', length: 150, nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  username: string;
+
+  @Column({ type: 'varchar', length: 150, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  password: string;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deleted_at: Date;
+}
