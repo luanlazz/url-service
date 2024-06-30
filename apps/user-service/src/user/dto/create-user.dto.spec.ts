1d60695693b02f21962ed2d89c8a95a3b7616886
luanlazz <luanlazzari@hotmail.com>
2024-06-30 16:06:53 -0300

    test(createUserDTO): should throw error if name is short or long

--- apps/user-service/src/user/dto/create-user.dto.spec.ts
+++ apps/user-service/src/user/dto/create-user.dto.spec.ts
@@ -2,6 +2,7 @@ import { validate } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 import { plainToInstance } from 'class-transformer';
 import { ValidationError } from '@nestjs/common';
+import { faker } from '@faker-js/faker';
 
 describe('CreateUserDto', () => {
   it('should be defined', () => {
@@ -18,7 +19,33 @@ describe('CreateUserDto', () => {
 
     const errors = await validate(myDtoObject);
 
-    expect(errors.length).not.toBe(0);
+    expect(errors.length).toBe(1);
+    expect(stringified(errors)).toContain(`name`);
+  });
+
+  it('should throw an error if name is short', async () => {
+    const userData = {
+      name: faker.string.alpha(1),
+    };
+
+    const myDtoObject = plainToInstance(CreateUserDto, userData);
+
+    const errors = await validate(myDtoObject);
+
+    expect(errors.length).toBe(1);
+    expect(stringified(errors)).toContain(`name`);
+  });
+
+  it('should throw an error if name is short', async () => {
+    const userData = {
+      name: faker.string.alpha(200),
+    };
+
+    const myDtoObject = plainToInstance(CreateUserDto, userData);
+
+    const errors = await validate(myDtoObject);
+
+    expect(errors.length).toBe(1);
     expect(stringified(errors)).toContain(`name`);
   });
 });
