bae52d9f5d9f19a3737cd39735ffe703bb1fbd06
luanlazz <luanlazzari@hotmail.com>
2024-07-01 03:22:53 -0300

    chore: remove unused file

--- apps/user-service/src/user/dto/validate-dto.ts
+++
@@ -1,9 +0,0 @@
-import { validate } from 'class-validator';
-
-export const validateDto = async (dto: any) => {
-  const errors = await validate(dto);
-  if (errors.length > 0) {
-    console.log('errors', errors);
-    throw new Error(errors.toString());
-  }
-};
