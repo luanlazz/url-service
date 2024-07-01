c6b8d5fc67935c0b4439b9d56bea05b918e6e647
luanlazz <luanlazzari@hotmail.com>
2024-07-01 04:01:10 -0300

    test(userService): should call hashing service when create a user

--- apps/user-service/src/user/user.service.spec.ts
+++ apps/user-service/src/user/user.service.spec.ts
@@ -1,18 +1,21 @@
-import { UniqueIdService } from '../../../../libs/unique-id/src';
-import { UserService } from './user.service';
 import { TestBed } from '@automock/jest';
-import { createUserDTOMockData } from './mocks/create-user.dto.mock';
 import { faker } from '@faker-js/faker';
+import { UserService } from './user.service';
+import { createUserDTOMockData } from './mocks/create-user.dto.mock';
+import { UniqueIdService } from '../../../../libs/unique-id/src';
+import { HashingService } from '../../../../libs/hashing/src';
 
 describe('UserService', () => {
   let service: UserService;
   let uniqueId: jest.Mocked<UniqueIdService>;
+  let hashingService: jest.Mocked<HashingService>;
 
   beforeEach(async () => {
     const { unit, unitRef } = TestBed.create(UserService).compile();
 
     service = unit;
     uniqueId = unitRef.get(UniqueIdService);
+    hashingService = unitRef.get(HashingService);
   });
 
   it('should be defined', () => {
@@ -28,5 +31,16 @@ describe('UserService', () => {
     service.create(createUserDto);
 
     expect(uniqueIdSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call hashing service once when creating a user', () => {
+    const createUserDto = createUserDTOMockData();
+    const hashingSpy = jest
+      .spyOn(hashingService, 'hash')
+      .mockReturnValue(new Promise((resolve) => resolve(faker.string.uuid())));
+
+    service.create(createUserDto);
+
+    expect(hashingSpy).toHaveBeenCalledTimes(1);
   });
 });
