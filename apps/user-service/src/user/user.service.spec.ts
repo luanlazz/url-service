38c6789ad1db4a93f5023dbf01af96ca3893815f
luanlazz <luanlazzari@hotmail.com>
2024-07-01 03:24:56 -0300

    test(userService): should call unique id when create a user

--- apps/user-service/src/user/user.service.spec.ts
+++ apps/user-service/src/user/user.service.spec.ts
@@ -1,18 +1,32 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { UniqueIdService } from '../../../../libs/unique-id/src';
 import { UserService } from './user.service';
+import { TestBed } from '@automock/jest';
+import { createUserDTOMockData } from './mocks/create-user.dto.mock';
+import { faker } from '@faker-js/faker';
 
 describe('UserService', () => {
   let service: UserService;
+  let uniqueId: jest.Mocked<UniqueIdService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
+    const { unit, unitRef } = TestBed.create(UserService).compile();
 
-    service = module.get<UserService>(UserService);
+    service = unit;
+    uniqueId = unitRef.get(UniqueIdService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should call uniqueId service once when creating a user', () => {
+    const createUserDto = createUserDTOMockData();
+    const uniqueIdSpy = jest
+      .spyOn(uniqueId, 'generate')
+      .mockReturnValue(faker.string.uuid());
+
+    service.create(createUserDto);
+
+    expect(uniqueIdSpy).toHaveBeenCalledTimes(1);
   });
 });
