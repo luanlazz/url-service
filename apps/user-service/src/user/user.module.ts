12d8e251827a333761b848dac28fe0176da96121
luanlazz <luanlazzari@hotmail.com>
2024-07-01 04:02:44 -0300

    chore(userService): hash password to persist new user

--- apps/user-service/src/user/user.module.ts
+++ apps/user-service/src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { UniqueIdModule } from 'libs/unique-id/src';
+import { UniqueIdModule } from '@app/unique-id';
+import { HashingModule } from 'libs/hashing/src';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), UniqueIdModule],
+  imports: [TypeOrmModule.forFeature([User]), UniqueIdModule, HashingModule],
   controllers: [UserController],
   providers: [UserService],
 })
--- apps/user-service/src/user/user.service.ts
+++ apps/user-service/src/user/user.service.ts
@@ -3,19 +3,23 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { UniqueIdService } from '../../../../libs/unique-id/src';
+import { HashingService } from '../../../../libs/hashing/src';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly uniqueId: UniqueIdService) {}
+  constructor(
+    private readonly uniqueId: UniqueIdService,
+    private readonly hashingService: HashingService,
+  ) {}
 
-  create(createUserDto: CreateUserDto): Promise<User> {
+  async create(createUserDto: CreateUserDto): Promise<User> {
     const user: User = new User();
 
     user.id = this.uniqueId.generate();
     user.name = createUserDto.name;
     user.email = createUserDto.email;
     user.username = createUserDto.username;
-    user.password = createUserDto.password;
+    user.password = await this.hashingService.hash(createUserDto.password);
   }
 
   findAll() {
