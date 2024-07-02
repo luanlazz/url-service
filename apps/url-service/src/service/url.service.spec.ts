e9a29ddb9029eaf3254d62af6659424bd0a82d69
luanlazz <luanlazzari@hotmail.com>
2024-07-02 04:56:01 -0300

    test(urlService): should call url repository to save new url

--- apps/url-service/src/service/url.service.spec.ts
+++ apps/url-service/src/service/url.service.spec.ts
@@ -3,16 +3,19 @@ import { UrlService } from './url.service';
 import { CreateURLDto } from '../dto/create-url.dto';
 import { faker } from '@faker-js/faker';
 import { UniqueIdService } from '../../../../libs/unique-id/src';
+import { UrlRepository } from '../repository/url.repository';
 
 describe('UrlService', () => {
   let service: UrlService;
   let uniqueIdService: jest.Mocked<UniqueIdService>;
+  let urlRepository: jest.Mocked<UrlRepository>;
 
   beforeEach(async () => {
     const { unit, unitRef } = TestBed.create(UrlService).compile();
 
     service = unit;
     uniqueIdService = unitRef.get(UniqueIdService);
+    urlRepository = unitRef.get(UrlRepository);
   });
 
   it('should be defined', () => {
@@ -29,5 +32,24 @@ describe('UrlService', () => {
 
     expect(uniqueIdService.generate).toHaveBeenCalledTimes(1);
     expect(uniqueIdService.generate).toHaveBeenCalledWith(6);
+  });
+
+  it('should call urlRepository save once', async () => {
+    const data: CreateURLDto = {
+      name: faker.lorem.words(),
+      url: faker.internet.url(),
+    };
+
+    const id = faker.string.uuid();
+    jest.spyOn(uniqueIdService, 'generate').mockReturnValue(id);
+
+    await service.createUrl(data);
+
+    expect(urlRepository.save).toHaveBeenCalledTimes(1);
+    expect(urlRepository.save).toHaveBeenCalledWith({
+      id,
+      name: data.name,
+      originalUrl: data.url,
+    });
   });
 });
