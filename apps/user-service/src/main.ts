380525490e2470411fb7c4567c3ce33be9dd20c1
luanlazz <luanlazzari@hotmail.com>
2024-07-01 22:25:44 -0300

    chore(userService): active Validation Pipe and use env variable in listen

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { UserServiceModule } from './user-service.module';
+import { ValidationPipe } from '@nestjs/common';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig({ path: '.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(UserServiceModule);
-  await app.listen(3011);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(process.env.PORT_USER_API);
 }
 bootstrap();
