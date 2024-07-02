408dff54d4747b58392d2fc76e3c995c31c0fba7
luanlazz <luanlazzari@hotmail.com>
2024-07-02 03:47:39 -0300

    chore(UrlService): active validation pipe and use env variable

--- apps/url-service/src/main.ts
+++ apps/url-service/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { UrlServiceModule } from './url-service.module';
+import { config as dotenvConfig } from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
+
+dotenvConfig({ path: '.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(UrlServiceModule);
-  await app.listen(3010);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen(process.env.PORT_URL_API);
 }
 bootstrap();
