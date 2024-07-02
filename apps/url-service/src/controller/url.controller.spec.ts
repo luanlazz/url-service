3b93c900a98eea354db7d96b0eb9a981cc53f433
luanlazz <luanlazzari@hotmail.com>
2024-07-02 06:04:42 -0300

    test(urlService): should service return the created url

--- apps/url-service/src/controller/url.controller.spec.ts
+++ apps/url-service/src/controller/url.controller.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@automock/jest';
 import { UrlController } from './url.controller';
 import { UrlService } from '../service/url.service';
+import { createUrlDTOMockData } from '../mocks/create-url.dto.mock';
 
 describe('UrlController', () => {
   let controller: UrlController;
@@ -18,11 +19,22 @@ describe('UrlController', () => {
   });
 
   it('should call service createUrl once', async () => {
-    const data = { name: 'test', url: 'http://test.com' };
+    const data = createUrlDTOMockData();
 
     await controller.create(data);
 
     expect(service.createUrl).toHaveBeenCalledTimes(1);
     expect(service.createUrl).toHaveBeenCalledWith(data);
+  });
+
+  it('should return the created url', async () => {
+    const data = createUrlDTOMockData();
+    const url = { id: '123', name: data.name, original_url: data.url };
+
+    jest.spyOn(service, 'createUrl').mockResolvedValue(url);
+
+    const result = await controller.create(data);
+
+    expect(result).toEqual(url);
   });
 });
